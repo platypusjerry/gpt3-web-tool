7b85ba6542a9fc6ccb03a0252f1428cfbb7e8662
Devanuj Baruah <45762816+platypusjerry@users.noreply.github.com>
2021-06-29 08:00:07 +0530

    update api_key

--- main_app.py
+++ main_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 from gpt3 import *
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 
